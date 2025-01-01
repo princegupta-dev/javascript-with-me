b6e8a0bb599fe5e2638f702e40f4d199655a7067
princegupta123 <princekumar39480@gmail.com>
2025-01-01 23:06:09 +0530

    push

--- javascript/prototype-and-inheritance/script.js
+++ javascript/prototype-and-inheritance/script.js
@@ -1,1 +1,12 @@
-console.log("Prototypes");
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}`);
+};
+
+const person1 = new Person("Prince");
+person1.greet(); // Output: Hello, my name is Prince
+console.log(person1.hasOwnProperty("name")); // true
+console.log(person1.hasOwnProperty("greet")); // false (inherited from prototype)
